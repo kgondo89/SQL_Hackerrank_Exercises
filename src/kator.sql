ebb48969f8c4ab96ebf50aaa02e86643b07fc5ab
kgondo89 <103499905+kgondo89@users.noreply.github.com>
2022-08-25 00:59:15 +0100

    Paste Hacker query

--- src/kator.sql
+++ src/kator.sql
@@ -6,3 +6,13 @@ where w.coins_needed = (select min(coins_needed)
                        on w2.code = wp2.code 
                        where wp2.is_evil = 0 and wp.age = wp2.age and w.power = w2.power)
 order by w.power desc, wp.age desc;
+
+
+SELECT c.hacker_id, h.name, count(c.challenge_id) 
+FROM Hackers AS h JOIN Challenges AS c ON h.hacker_id = c.hacker_id
+GROUP BY c.hacker_id, h.name 
+HAVING count(c.challenge_id) = (SELECT TOP 1 count(c1.challenge_id) FROM Challenges AS c1 GROUP BY c1.hacker_id 
+              ORDER BY count(*) desc) or
+count(c.challenge_id) NOT IN (SELECT count(c2.challenge_id) FROM Challenges AS c2 GROUP BY c2.hacker_id 
+            HAVING c2.hacker_id <> c.hacker_id)
+ORDER BY count(c.challenge_id) DESC, c.hacker_id;
